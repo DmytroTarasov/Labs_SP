898fa4c74440fba1f95afc7d5262c2a3558a8eb9
DmytroTarasov <65859911+DmytroTarasov@users.noreply.github.com>
2021-02-23 15:45:48 +0000

    Add files via upload

---
+++ Gender.h
@@ -0,0 +1,12 @@
+#ifndef UNTITLED_GENDER_H
+#define UNTITLED_GENDER_H
+
+enum Gender {
+    Male,
+    Female,
+    Else
+};
+
+    /*static const Gender AllGen[] = { Male, Female, Else};*/
+
+#endif //UNTITLED_GENDER_H
---
+++ Position.h
@@ -0,0 +1,14 @@
+#ifndef UNTITLED_POSITION_H
+#define UNTITLED_POSITION_H
+
+enum Position {
+    Director = 100,
+    Manager = 75,
+    OfficeWorker = 50,
+    Consultant = 25,
+    Unemployed = 0
+};
+
+   /*static const Position AllPos[] = { Director, Manager, OfficeWorker,
+                                       Consultant,  Unemployed};*/
+#endif //UNTITLED_POSITION_H
